aeb8c1124d59e407010b18ea0694613d36747872
Aubrey Agub <2555752a@student.gla.ac.uk>
2025-02-15 18:34:41 +0000

    refactored evaluation - added score direction checker, changed to minimum threshold checker

--- fmd_evolution/evaluation_strategy.py
+++ fmd_evolution/evaluation_strategy.py
@@ -3,43 +3,87 @@ import torch.nn.functional as F
 
 
 class EvaluationStrategy:
-    def __init__(self,probable_threshold=0.8,functional_threshold=0.3):
-        self.probable_threshold = probable_threshold
-        self.functional_threshold = functional_threshold
-    
-    def is_sequence_probable(self,sequence):
-        sequence_only_logits = sequence.sequence_only_logits
-        log_p = torch.log(sequence_only_logits) # epsilon to avoid nan
+    def __init__(self,root_sequence,p_tolerance=0.1,f_tolerance=0.5):
+        self.root_sequence = root_sequence
+        self.p_tolerance = p_tolerance
+        self.f_tolerance = f_tolerance
+        self.root_p = None
+        self.root_p = self.get_sequence_probability(self.root_sequence)
+        self.min_p = self.root_p - self.p_tolerance
+        self.min_s_score = 1 - self.f_tolerance
+
+    def get_sequence_probability(self,sequence):
+        sequence_aa_logits = sequence.sequence_aa_logits
+        log_p = torch.log(sequence_aa_logits) # epsilon to avoid nan
         mean_log_p = torch.mean(log_p)
         sequence_p = torch.exp(mean_log_p)
-        return sequence_p>self.probable_threshold,sequence_p
+        return sequence_p
+    
+    def is_sequence_functional(self,sequence_p): # sequence probability as approximation of fitness
+        return self.min_p <= sequence_p
+    
+    def is_sequence_probability_increasing(self, seq_p,parent_seq_p):
+        return seq_p>parent_seq_p 
 
-    #     # set, then in another func compare change from previous sequence
-    #     # other ascpects to evaluate: increase protein fitness + antigenic pressure if applicable + functional + env via past data
-
-    def is_sequence_functional(self,sequence):
+    def get_sequence_structure_score(self,sequence,parent_sequence):
         sequence_embedding = sequence.embeddings
-        previous_sequence_embedding = sequence.parent_seqs[-1].embeddings
+        parent_sequence_embedding = parent_sequence.embeddings
         # compare with previous embeddings using a distance metric: cosine similarity
         # cosine_sim = F.cosine_similarity(sequence_embedding,previous_sequence_embedding,dim=1)
         # mean_cosine_sim = torch.mean(cosine_sim).item()
         # print(f"Cosine similarity = {mean_cosine_sim}")
         # return mean_cosine_sim> self.functional_threshold,mean_cosine_sim
-        l2_distance = torch.norm(sequence_embedding - previous_sequence_embedding, p=2, dim=1)
+        l2_distance = torch.norm(sequence_embedding - parent_sequence_embedding, p=2, dim=1)
         mean_l2_distance = torch.mean(l2_distance).item()
         functional_score = 1 - mean_l2_distance # so both metrics are in the same direction
-        return functional_score>self.functional_threshold,functional_score
+        return functional_score
+
+    def is_sequence_structurally_similar(self,sequence_functional_score):
+        return self.min_s_score <= sequence_functional_score 
     
-    def should_continue_mutating(self,sequence):  ######### refactor to only check threshold
-        is_probable,sequence_p = self.is_sequence_probable(sequence)
-        is_functional,cosine_sim = self.is_sequence_functional(sequence)
-        sequence.mutation_score = (sequence_p + cosine_sim)/2 # average of scores
-        if is_probable and is_functional:
+    def is_sequence_structure_increasing(self,seq_f_score,parent_seq_f_score):
+        return seq_f_score>parent_seq_f_score
+    
+    def get_sequence_scores(self,sequence,parent_sequence):
+        sequence_p = self.get_sequence_probability(sequence)
+        sequence_f_score = self.get_sequence_structure_score(sequence,parent_sequence)
+        return sequence_p,sequence_f_score
+
+    def get_parent_scores(self,parent_sequence):
+        parent_p = self.get_sequence_probability(parent_sequence)
+        if parent_sequence.parent_seqs:
+            parent_f_score = self.get_sequence_structure_score(parent_sequence,parent_sequence.parent_seqs[-1])
+        else:
+            parent_f_score = 0 # root node does not have a parent
+        return parent_p,parent_f_score
+
+     # create new function to combine score and call in evolution
+    def set_mutation_score(self,sequence,parent_sequence):
+        sequence_p,sequence_functional_score = self.get_sequence_scores(sequence,parent_sequence)
+        average_score = (sequence_p + sequence_functional_score)/2
+        sequence.set_mutation_score(average_score) 
+        return None
+    
+    def should_accept_mutated_sequence(self,sequence,parent_sequence):
+        sequence_p,sequence_f_score = self.get_sequence_scores(sequence,parent_sequence)
+        is_functional = self.is_sequence_functional(sequence_p)
+        is_sequence_structurally_similar = self.is_sequence_structurally_similar(sequence_f_score)
+        return is_functional and is_sequence_structurally_similar
+
+    def should_continue_mutating(self,sequence,parent_sequence):  ######### refactor to only check threshold
+        sequence_p,sequence_f_score = self.get_sequence_scores(sequence,parent_sequence)
+        parent_sequence_p,parent_f_score = self.get_parent_scores(parent_sequence)
+
+        is_probability_increasing = self.is_sequence_probability_increasing(sequence_p,parent_sequence_p)
+        is_sequence_structure_increasing = self.is_sequence_structure_increasing(sequence_f_score,parent_f_score)
+
+        if is_probability_increasing and is_sequence_structure_increasing:
             return True 
         else:
             return False
     
-    # create new function to combine score and call in evolution
         
-
-    # next steps: instead of just checking it falls above a threshold, I need to cehck the direction of change (increased or decrese) to correspond with increased or decreased fitness
+        
+    # other ascpects to evaluate: increase protein fitness + antigenic pressure if applicable + env via past data
+    
+    
