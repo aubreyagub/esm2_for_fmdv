e0f3639c93f12f38d1fff9e399ef620303a7e6c5
Aubrey Agub <2555752a@student.gla.ac.uk>
2025-03-13 21:19:24 +0000

    applied softmax to mutation scores when validating-selecting mutations from the pool to treat as a probability distribution

--- fmd_evolution/ranked_evaluation_strategy.py
+++ fmd_evolution/ranked_evaluation_strategy.py
@@ -4,11 +4,12 @@ import torch.nn.functional as F
 from . import SEED, rng
 
 class RankedEvaluationStrategy:
-    def __init__(self,root_sequence):
+    def __init__(self,root_sequence,num_of_mutations_desired=3):
         self.root_sequence = root_sequence
         self.root_p = None
         self.root_p = self.get_sequence_probability(self.root_sequence)
-
+        self.num_of_mutations_desired = num_of_mutations_desired
+        
     def get_sequence_probability(self,sequence):
         sequence_aa_logits = sequence.sequence_aa_logits
         log_p = torch.log(sequence_aa_logits)
@@ -37,12 +38,16 @@ class RankedEvaluationStrategy:
     
     def get_ranked_mutation_scores(self,mutated_nodes,parent_sequence):
         seq_p_list,cosine_sim_list = self.get_mutation_scores(mutated_nodes,parent_sequence)
+        mutation_data = list(zip(mutated_nodes,seq_p_list,cosine_sim_list))
+        
         # rank the mutations based on the two metrics
-        mutated_seqs_sorted_by_p = sorted(mutated_nodes,  key=lambda item: seq_p_list, reverse=True) # higher probability is better 
-        mutated_seqs_sorted_by_cs = sorted(mutated_nodes,  key=lambda item: cosine_sim_list, reverse=True) # higher cosine similarity is better 
+        mutated_seqs_sorted_by_p = sorted(mutation_data,  key=lambda x:x[1], reverse=True) # higher probability is better 
+        p_ranks ={mutation[0]:rank for rank,mutation in enumerate(mutated_seqs_sorted_by_p)}
+        mutated_seqs_sorted_by_cs = sorted(mutation_data,  key=lambda x:x[2]) # lower cosine similarity is better 
+        cs_ranks ={mutation[0]:rank for rank,mutation in enumerate(mutated_seqs_sorted_by_cs)}
 
         # calculate the mutation scores based on rank averages; rank 1 is the best mutation
-        mutation_scores = [mutated_seqs_sorted_by_p.index(seq)+mutated_seqs_sorted_by_cs.index(seq) for seq in mutated_nodes]
+        mutation_scores = [p_ranks[seq]+cs_ranks[seq] for seq in mutated_nodes]
         return mutation_scores
     
     def set_ranked_mutation_scores(self,mutated_nodes,mutation_scores):
@@ -51,16 +56,17 @@ class RankedEvaluationStrategy:
             
         return 
     
+    def apply_softmax_to_ranked_scores(self, mutation_scores):
+        mutation_scores_torch = torch.tensor(mutation_scores,dtype=torch.float32)
+        softmax = torch.nn.Softmax(dim=0)
+        softmax_scores  = softmax(-mutation_scores_torch) # negative to give ranked scores closer to 0 a probability closer to 1, and T =1 
+        return softmax_scores.numpy()
     
     def get_viable_mutations(self,potential_mutations,parent_sequence):
-        parent_sequence_mutation_score = parent_sequence.mutation_score
-        viable_mutations = []
-        for sequence in potential_mutations:
-            sequence_mutation_score = sequence.mutation_score
-            if sequence_mutation_score>=parent_sequence_mutation_score: 
-                viable_mutations.append(sequence) # guaranteed mutation if beneficial change
-            else:
-                # convert to metropolis hastings later
-                continue
-
+        mutation_scores = self.get_ranked_mutation_scores(potential_mutations,parent_sequence)
+        print(f"Mutation scores: {mutation_scores}")
+        softmax_scores = self.apply_softmax_to_ranked_scores(mutation_scores)
+        print(f"Softmax scores: {softmax_scores}")
+        num_of_mutations_desired = min(self.num_of_mutations_desired,len(potential_mutations))
+        viable_mutations = rng.choice(potential_mutations,num_of_mutations_desired,replace=False,p=softmax_scores)
         return viable_mutations
