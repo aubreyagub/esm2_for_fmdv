72ff55d8f6e975931e53bb6decee97e1fa51ccd0
Aubrey Agub <2555752a@student.gla.ac.uk>
2025-03-16 22:38:51 +0000

    add pareto check to get viable mutations directly based on probability and distance, instead of mutation score ranking

--- fmd_evolution/ranked_evaluation_strategy.py
+++ fmd_evolution/ranked_evaluation_strategy.py
@@ -46,7 +46,7 @@ class RankedEvaluationStrategy:
         mutated_seqs_sorted_by_cs = sorted(mutation_data,  key=lambda x:x[2]) # lower cosine similarity is better 
         cs_ranks ={mutation[0]:rank for rank,mutation in enumerate(mutated_seqs_sorted_by_cs)}
 
-        # calculate the mutation scores based on rank averages; rank 1 is the best mutation
+        # calculate the mutation scores based on rank averages; rank 0 is the best mutation
         mutation_scores = [p_ranks[seq]+cs_ranks[seq] for seq in mutated_nodes]
         return mutation_scores
     
@@ -56,6 +56,13 @@ class RankedEvaluationStrategy:
             
         return 
     
+    def set_probability_and_embedding_distance(self,mutated_nodes,parent_sequence):
+        seq_p_list,cosine_sim_list = self.get_mutation_scores(mutated_nodes,parent_sequence) # unranked scores
+        for sequence,seq_p,cosine_sim in zip(mutated_nodes,seq_p_list,cosine_sim_list):
+            sequence.probability = seq_p
+            sequence.embedding_distance = cosine_sim
+        return
+    
     def apply_softmax_to_ranked_scores(self, mutation_scores):
         mutation_scores_torch = torch.tensor(mutation_scores,dtype=torch.float32)
         softmax = torch.nn.Softmax(dim=0)
@@ -63,10 +70,33 @@ class RankedEvaluationStrategy:
         return softmax_scores.numpy()
     
     def get_viable_mutations(self,potential_mutations,parent_sequence):
-        mutation_scores = self.get_ranked_mutation_scores(potential_mutations,parent_sequence)
-        print(f"Mutation scores: {mutation_scores}")
-        softmax_scores = self.apply_softmax_to_ranked_scores(mutation_scores)
-        print(f"Softmax scores: {softmax_scores}")
-        num_of_mutations_desired = min(self.num_of_mutations_desired,len(potential_mutations))
-        viable_mutations = rng.choice(potential_mutations,num_of_mutations_desired,replace=False,p=softmax_scores)
+        # simple check for improvement
+        mutation_scores = [mutation.mutation_score for mutation in potential_mutations]
+        parent_mutation_score = parent_sequence.mutation_score
+        print(f"Mutation score of parent sequence = {parent_mutation_score}")
+        print(f"Mutation scores of potential mutations = {mutation_scores}")
+        viable_mutations = [mutation for mutation in potential_mutations if mutation.mutation_score<=parent_sequence.mutation_score] # will continue choosing top rank mutation
+
+        # # sampling using softmax_scores
+        # mutation_scores = self.get_ranked_mutation_scores(potential_mutations,parent_sequence)
+        # print(f"Mutation scores: {mutation_scores}")
+        # softmax_scores = self.apply_softmax_to_ranked_scores(mutation_scores)
+        # print(f"Softmax scores: {softmax_scores}")
+        # num_of_mutations_desired = min(self.num_of_mutations_desired,len(potential_mutations))
+        # viable_mutations = rng.choice(potential_mutations,num_of_mutations_desired,replace=False,p=softmax_scores)
+        return viable_mutations
+    
+    def get_viable_mutations_via_pareto(self,potential_mutations,parent_sequence):
+        viable_mutations = []
+        for mutation in potential_mutations:
+            mutation_probability = mutation.probability
+            mutation_embedding_distance = mutation.embedding_distance
+            parent_probability = parent_sequence.probability
+            parent_embedding_distance = parent_sequence.embedding_distance
+            print(f"Parent mutation: {parent_sequence.id}, probability: {parent_probability}, distance: {parent_embedding_distance}")
+            print(f"Mutation: {mutation.id}, probability: {mutation_probability}, distance: {mutation_embedding_distance}")
+
+            if mutation_probability>=parent_probability or mutation_embedding_distance<=parent_embedding_distance:
+                viable_mutations.append(mutation)
+
         return viable_mutations
